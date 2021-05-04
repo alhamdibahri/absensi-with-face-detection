df5d5ee7ae4065fa87770079115a78d6b0156958
ardenta.world <ardenta.world@gmail.com>
2021-05-04 19:41:25 +0700

    add-adminlte

--- public/script.js
+++ public/script.js
@@ -9,43 +9,32 @@ function dataURLtoBlob(dataurl) {
 
 var video = document.querySelector("#video-webcam");
 
-    // minta izin user
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-    // jika user memberikan izin
     if (navigator.getUserMedia) {
-        // jalankan fungsi handleVideo, dan videoError jika izin ditolak
         navigator.getUserMedia({ video: true }, handleVideo, videoError);
     }
 
-    // fungsi ini akan dieksekusi jika  izin telah diberikan
     function handleVideo(stream) {
         video.srcObject = stream;
     }
 
-    // fungsi ini akan dieksekusi kalau user menolak izin
     function videoError(e) {
-        // do something
         alert("Izinkan menggunakan webcam untuk demo!")
     }
 
   function takeSnapshot() {
-      // buat elemen img
       var img = document.createElement('img');
       img.setAttribute("id", "hasil-image");
       var context;
   
-      // ambil ukuran video
       var width = video.offsetWidth
               , height = video.offsetHeight;
   
-      // buat elemen canvas
       canvas = document.createElement('canvas');
       canvas.width = width;
       canvas.height = height;
-  
-      // ambil gambar dari video dan masukan 
-      // ke dalam canvas
+
       context = canvas.getContext('2d');
       context.drawImage(video, 0, 0, width, height);
       
@@ -75,7 +64,6 @@ async function start() {
   let canvas
   
   imageUpload.addEventListener('click', async () => {
-    /* Dijalankan ketika terjadi file upload  di sini pencocokan data di lakukan*/
     if (image) image.remove()
     if (canvas) canvas.remove()
     image = await faceapi.bufferToImage(takeSnapshot())
